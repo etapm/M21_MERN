574c6d0a90ae002981c6bb29bb4209437662d7ac
etapm <etap@ucdavis.edu>
2023-05-23 21:44:44 -0700

    apolo server dependency alignment

---
+++ client/src/utils/queries.js
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const GET_ALL_BOOKS = gql`
+  {
+    books {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+`;
